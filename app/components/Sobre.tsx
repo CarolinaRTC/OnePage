4999f95e0afbd10196685330bf18f5754a6c6d1e
Hannah <hannah.clara.so@gmail.com>
2023-07-25 17:24:07 -0300

    Alteração da padding bottom dentro da seção 'sobre'

--- app/components/Sobre.tsx
+++ app/components/Sobre.tsx
@@ -9,13 +9,15 @@ export const Sobre = () => {
           Somos uma empresa com o objetivo de capturar criaturas prejudiciais a jardins e plantações, sendo nosso maior foco os coelhos.</p>
           <p className="text-base text-center font-montserrat">Nossa equipe é excepcionalmente qualificada e utiliza apenas ferramentas autorais, tecnologias desenvolvidas pelas nossas próprias mãos e confiáveis mesmo nas situações mais difíceis.</p>
 
-          <p className="text-base text-center font-montserrat pb-1">Nossa empresa funciona bem em razão da forte base que seguimos: 
-          <ul>
-            <li className="text-xl font-italic font-bold">safe,</li>
-            <li className="text-xl font-italic font-bold">secure,</li>
-            <li className="text-xl font-italic font-bold">humane.</li>
-          </ul></p>
-            <p className="text-base text-center font-montserrat pb-4">Acreditamos que um serviço bem-feito é aquele em que todas as partes envolvidas saem ganhando, por isso a comunicação com os clientes é muito importante para nós.</p>
+          <p className="text-base text-center font-montserrat pb-2">Nossa empresa funciona bem em razão da forte base que seguimos: </p>
+          <div className="flex justify-center bg-green-800 rounded-md">
+            <ul className=" text-xl text-gray-100 text-center font-montserrat pb-1">
+              <li className="text-xl font-italic font-bold">safe,</li>
+              <li className="text-xl font-italic font-bold">secure,</li>
+              <li className="text-xl font-italic font-bold">humane.</li>
+            </ul>
+          </div>
+            <p className="text-base text-center font-montserrat pt-2 pb-4">Acreditamos que um serviço bem-feito é aquele em que todas as partes envolvidas saem ganhando, por isso a comunicação com os clientes é muito importante para nós.</p>
           <div className="flex items-center justify-center place-content-center">
             <img
             src="img/WGterno.png"
